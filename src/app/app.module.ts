4d14ff960fb330432772d85dbddca01bace6ee06
mehak <mdua798@gmail.com>
2020-03-04 14:41:23 +0530

    Fix styles and remove unwanted files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {RouterModule,Route} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { ShowListComponent } from './show/components/show-list/show-list.component';
 import { SharedModule } from './shared/shared.module';
-const routes:Route[]=[
-  { path:'',
-   component:ShowListComponent
-  }
-]
-  
+
 @NgModule({
   declarations: [
     AppComponent,
